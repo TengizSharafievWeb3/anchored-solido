8bd063466678a22b815dcfa822cc287056dc548b
Tengiz Sharafiev <btolfa@gmail.com>
2022-03-15 08:06:44 +0700

    fix fmt

--- programs/asolido/src/process_validator.rs
+++ programs/asolido/src/process_validator.rs
@@ -1,13 +1,15 @@
+use crate::state::Validator;
 use crate::AddValidator;
 use anchor_lang::prelude::*;
-use crate::state::Validator;
 
 impl<'info> AddValidator<'info> {
     pub fn process(&mut self) -> Result<()> {
         let lido = &mut self.lido;
-        lido.validators.add(
-            self.validator_vote.key(),
-            Validator::new(self.validator_fee_st_sol.key()))
+        lido.validators
+            .add(
+                self.validator_vote.key(),
+                Validator::new(self.validator_fee_st_sol.key()),
+            )
             .map_err(|err| error!(err))
     }
 }
